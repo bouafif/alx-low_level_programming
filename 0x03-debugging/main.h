c9dbaf576b0c80788a1468c9a9386051cfba941c
bouafif <bouafifwael12@gmail.com>
2022-12-15 05:47:01 -0800

    task0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,28 +1,9 @@
-fndef MAIN_H                                                                                                                                           
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
-#define MAIN_H                                                                                                                                           
-
-void print_alphabet(void);                                                                                                                               
-
-void print_alphabet_x10(void);                                                                                                                           
-
-int _islower(int c);                                                                                                                                     
-
-int _isalpha(int c);                                                                                                                                     
-
-int print_sign(int n);                                                                                                                                   
-
-int _abs(int);                                                                                                                                           
-
-int print_last_digit(int);                                                                                                                               
-
-void jack_bauer(void);                                                                                                                                   
-
-void times_table(void);                                                                                                                                  
-
-int add(int, int);                                                                                                                                       
-
-void print_to_98(int n);                                                                                                                                 
-
-int _putchar (char);                                                                                                                                     
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int conver_day(int month, int day);
 #endif
