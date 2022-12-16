1517cab3f8793f9be3f3d0cf394381506cd97aa5
bouafif <bouafifwael12@gmail.com>
2022-12-15 18:38:16 -0800

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line according to parameter
+ * @n: the number of lines to draw
+ * Return: empty
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = x; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
