15715c09936bc931fb0e1407ccf63f1a8a443589
bouafif <bouafifwael12@gmail.com>
2023-01-04 13:19:33 -0800

    5_sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,17 @@
-include "main.h"
+#include "main.h"
 /**
- * sqart - Makes possible to evaluate from 1 to n
+ * sqrt2 - Makes possible to evaluate from 1 to n
  * @a: same number as n
  * @b: number that iterates from 1 to n
  * Return: 1 on success , -1 on fail
  */
-int sqart(int a, int b)
+int sqrt2(int a, int b)
 {
 	if (b * b == a)
 		return (b);
 	else if (b * b > a)
 		return (-1);
-	return (sqart(a, b + 1));
+	return (sqrt2(a, b + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of n
@@ -20,5 +20,5 @@ int sqart(int a, int b)
  */
 int _sqrt_recursion(int n)
 {
-	return (sqart(n, 1));
+	return (sqrt2(n, 1));
 }
